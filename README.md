0e1f615c3611ca1512e1c377b6222bafd0aa6324
leocavalcante <leonardo.cavalcante@picpay.com>
2023-04-12 16:30:56 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Otel PHP Metrics
+
+Very small example aplication to debug HTTP requests not summing up.
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+use OpenTelemetry\API\Common\Signal\Signals;
+use OpenTelemetry\Contrib\Grpc\GrpcTransportFactory;
+use OpenTelemetry\Contrib\Otlp\MetricExporter;
+use OpenTelemetry\SDK\Common\Export\Stream\StreamTransport;
+use OpenTelemetry\SDK\Common\Time\ClockFactory;
+use OpenTelemetry\SDK\Metrics\MeterProvider;
+use OpenTelemetry\SDK\Metrics\MetricReader\ExportingReader;
+use OpenTelemetry\Contrib\Otlp\OtlpUtil;
+
+require 'vendor/autoload.php';
+
+$transport = (new GrpcTransportFactory())->create('http://localhost:4317' . OtlpUtil::method(Signals::METRICS));
+$exporter = new MetricExporter($transport);
+$reader = new ExportingReader($exporter, ClockFactory::getDefault());
+
+$meter = MeterProvider::builder()
+       ->addReader($reader)
+       ->build()
+       ->getMeter('example');
+
+$counter = $meter->createCounter('http_requests');
+
+$counter->add(1);
+
+$reader->collect();
